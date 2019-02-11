23a53ea54295757baac086f40dfea898ed345f51
Dennis Garcia <dennis.garcia.28@lacetania.cat>
2019-02-11 11:22:20 +0100

    Actualització del Prog.03

--- 05. Comunicació Sèrie Avançada/Prog.03/Prog.03.ino
+++ 05. Comunicació Sèrie Avançada/Prog.03/Prog.03.ino
@@ -1,6 +1,5 @@
 //********** Variables ********************************************************
 float r1, r2;
-// variables for the results
 float rSerie, rParalel;
 
 //********** Setup ****************************************************************
@@ -12,33 +11,33 @@ void setup() {
 
 //********** Loop *****************************************************************
 void loop() {
-  // if there's any serial available, read it:
+  
   while (Serial.available() > 0) {
 
-    // look for the next valid integer in the incoming serial stream:
-    r1 = Serial.parseInt(); 
+    r1 = Serial.parseInt();
+    r1 = 400; 
     Serial.print("r1 = ");
     Serial.print(r1);
     Serial.print(" ohms\t");
-    // do it again:
-    r2 = Serial.parseInt(); 
+    
+    r2 = Serial.parseInt();
+    r2 = 500; 
     Serial.print("r2 = ");
     Serial.print(r2);
     Serial.println(" ohms");
 
-    // look for the newline. That's the end of your sentence:
     if (Serial.read() == '\n') {
-      // calculate serie and paralel
-      rSerie = r1 + r2;
-      rParalel = (r1 * r2) / (r1 + r2);
       
+      rSerie = r1 + r2;      
       Serial.print("rSerie = ");
       Serial.print(rSerie);
       Serial.print(" ohms\t");
+
+      rParalel = (r1 * r2) / (r1 + r2);
       Serial.print("rParal.lel = ");
       Serial.print(rParalel);
       Serial.println(" ohms");
-      Serial.println();
+     
       Serial.println("Entra nous valors per r1 i r2 (separats per una coma)");
     }
   }
